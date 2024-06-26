e63fdb1c4eaa8bb4518ae75d4a73d561765dcead
hidopinto <hidopinto.1@gmail.com>
2024-06-27 02:46:04 +0300

    returned inner lgbm models (#3)

--- backbone/predict_mortality.py
+++ backbone/predict_mortality.py
@@ -62,12 +62,12 @@ def main():
     print('training pred model wo blood tests')
     pred_wo_bt_model = train_without_blood_tests(data, conf)[OUTCOME_COL]
     print('saving pred model wo blood tests')
-    joblib.dump(pred_wo_bt_model, '/home/yehduap/Desktop/SegalLab/dzhAI/output/models/pred_wo_bt_model.joblib')
+    joblib.dump(pred_wo_bt_model.trained_model, '/home/yehduap/Desktop/SegalLab/dzhAI/output/models/pred_wo_bt_model.joblib')
 
     print('training pred model with blood tests')
     pred_bt_model = train_with_blood_tests(data, conf)[OUTCOME_COL]
     print('saving pred model with blood tests')
-    joblib.dump(pred_bt_model, '/home/yehduap/Desktop/SegalLab/dzhAI/output/models/pred_bt_model.joblib')
+    joblib.dump(pred_bt_model.trained_model, '/home/yehduap/Desktop/SegalLab/dzhAI/output/models/pred_bt_model.joblib')
 
 
 if __name__ == '__main__':
